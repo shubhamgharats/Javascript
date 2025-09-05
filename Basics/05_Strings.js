9f30dd585b15d646fdbe909d97027e423fe22d51
shubhamgharats <gharatshubham306@gmail.com>
2025-09-05 19:47:52 +0000

    Strings

---
+++ Basics/05_Strings.js
@@ -0,0 +1,27 @@
+const name = "Hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value")
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = newString('Hiteshc')
+
+console.log(gameName[0])
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+console.log(newString);
+console.log(anotherString);
+console.log(url.replace('%20','-'))
+console.log(url.includes(sundar))
+console.log(gameName.split('-'))
+
+const newString = gameName.substring(0,4)
+
+const anotherString = gameName.slice(0,4)
+
+const url = "https://www.youtube.com/shubham%20gharat"
+

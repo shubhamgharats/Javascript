38df177ca90ef9c40968adcb78a56daf61424d33
shubhamgharats <gharatshubham306@gmail.com>
2025-09-05 14:37:24 +0000

    Conversion of DataTypes

---
+++ Basics/03_Conversion.js
@@ -0,0 +1,22 @@
+// let score = undefined
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// let isLoggedIn = 0
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log( booleanIsLoggedIn )
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+

656b61dbb73d6ae2b925ce2b1a4380419df84bc7
shubhamgharats <gharatshubham306@gmail.com>
2025-09-05 18:49:23 +0000

    DataTypes Summary

---
+++ Basics/datatypes-summary.js
@@ -0,0 +1,23 @@
+ const score = 100
+ const scoreValue = 100.3
+
+ const isLoggedIn = false
+ const outsideTemp = null
+ let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const heros = ["Shakitiman","Naagraj","Doga"];
+let myObj ={
+    name: "Hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+ console.log("Hello World");
+}
+
+console.log(typeof anotherId);

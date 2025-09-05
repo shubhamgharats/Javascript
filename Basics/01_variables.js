1ba08924e835e3ece8757f0b059c0ee9eabf033e
shubhamgharats <gharatshubham306@gmail.com>
2025-09-05 09:50:17 +0000

    dataTypes

---
+++ Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 14434 
+let accountEmail = "Example@gmail.com" 
+var accountPassword="12345" 
+let accountState
+
+accountEmail = "bhdbcdc@gmail.com"
+accountPassword = "1323231331"
+accountCity = "Bengluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword, accountCity,accountState])
---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// console.log(3+3)
+// console.log("Hitesh")
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false
+
+console.log(typeof "hitesh")
+console.log(typeof null)

59789f16b6b47f0cfce99ded6902995d0dfd1627
shubhamgharats <gharatshubham306@gmail.com>
2025-09-06 13:11:06 +0000

    Array1

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,38 @@
+// array
+
+const myArr = [0,1,2,3,4,5,6,"Hello", "Love"]
+const myHeros = ["Shaktiman", "Naagraj"]
+
+const myArr2 = new Array(1,2,3,4)
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// Slice, Splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1)
+
+console.log("B ", myArr);
+
+const myn2 = console.log("B ", myArr);
+
+console.log("C ", myArr);
+console.log(myn2);
